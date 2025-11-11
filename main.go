054f10108c5f2b827109c931de5681727f62a37f
Daniel Steinhauer <d.steinhauer@mailbox.org>
2025-11-11 16:49:34 +0100

    Remove strategy.

--- main.go
+++ main.go
@@ -5,24 +5,14 @@ import (
 	"log"
 
 	"github.com/Pik-9/SilphScope/src/repository"
-	"github.com/Pik-9/SilphScope/src/strategy"
 )
 
 func main() {
 	commitHash := flag.String("c", "HEAD", "The commit to unghost.")
 	repoPath := flag.String("r", ".", "Path to repository.")
-	stratStr := flag.String("u", "author", `Strategy to unghost [author|commit].
-  Strategy author will create one commit per author while discarding original commit message and date.
-  Strategy commit will recreate every commit with its message and date.`)
 	flag.Parse()
 
-	strat, err := strategy.New(*stratStr)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Default().Println("Unghosting commit", *commitHash, "in repo", *repoPath, "while using strategy", strat)
+	log.Default().Println("Unghosting commit", *commitHash, "in repo", *repoPath)
 
 	patch, commit, parent, repo, err := repository.ExtractPatch(*repoPath, *commitHash)
 	if err != nil {
